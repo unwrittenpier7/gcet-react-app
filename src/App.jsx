03b02419fbf8c1b913b598ed7a46d29c2cbb8971
unwrittenpier7 <itsmepeter143@gmail.com>
2025-05-21 20:33:17 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Ecommerce Page</h1>
+      <hr />
+      <h2>Product List</h2>
+      <hr />
+      <footer>
+        <h2>Footer</h2>
+      </footer>
     </>
   )
 }
