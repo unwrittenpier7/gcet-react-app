8fe16f53ba39db95016ae87b36761c987d26cd55
unwrittenpier7 <itsmepeter143@gmail.com>
2025-06-01 22:39:37 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import Product from "./assets/components/product.jsx";
-import Cart from "./assets/components/cart.jsx";
-import login from "./assets/components/login.jsx";
-
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Logout from "./components/Logout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext } from "react";
+export const AppContext = createContext();
 function App() {
-  
-
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   return (
     <div>
-      <BrowserRouter>
-        <header>
-          <h1>Ecommerce Shop</h1>
-          <Link to="/">Home</Link>-
-          <Link to="/cart">Cart</Link>
-          <hr />
-        </header>
-
-        <main>
+      <AppContext.Provider value={{ users, setUsers, user, setUser }}>
+        <BrowserRouter>
+          <Header />
           <Routes>
             <Route index element={<Product />} />
-            <Route path="/" element={<Product/>}></Route>
-            <Route path="/cart" element={<Cart/>}></Route>
+            <Route path="/" element={<Product />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/logout" element={<Logout />}></Route>
+            <Route path="/register" element={<Register />}></Route>
           </Routes>
-        </main>
-
-        <footer>
-          <hr />
-          &copy; 2005. All rights Reserved.
-        </footer>
-      </BrowserRouter>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
     </div>
   );
 }
-
-export default App
+export default App;
--- src/assets/components/product.jsx
+++ src/assets/components/product.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import { AppContext } from '../App'
 export default function Product() {
+  const {user} = useContext(AppContext)
   return (
-    <div>Product List</div>
+    <div>
+      <h3>Welcome {user.name}! </h3>
+      Product List
+      </div>
   )
 }
